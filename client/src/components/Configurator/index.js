633df2ff3660a8d40d6df8931c3611900d9baabc
vileluv <s.kolupov@icloud.com>
2025-05-11 15:50:00 +0300

    add payments

--- client/src/components/Configurator/index.js
+++ client/src/components/Configurator/index.js
@@ -11,6 +11,7 @@ import Button from "../common/Button/index.js";
 import HardwareComponent from "../HardwareComponent/index.js";
 import { reaction } from "mobx";
 import Modal from "../Modal/index.js";
+import multiModuleStyles from "../../utility/multiModuleStyles.js";
 function Configurator() {
     const [confCode, setConfCode] = useState("");
     const confCodeRef = useRef(confCode);
@@ -187,6 +188,8 @@ function Configurator() {
                                     )}
                                     ₽
                                 </div>
+                                <div className={styles.paymenttitle}>Оплата</div>
+                                <PaymentList />
                                 <Button className={styles.modalbtn}>Купить</Button>
                             </div>
                         </Modal>
@@ -198,3 +201,94 @@ function Configurator() {
 }
 
 export default observer(Configurator);
+function PaymentItem({ children, title, description, onClick, picture = false, isActive = false, ...props }) {
+    const item = useRef();
+    return (
+        <div
+            ref={item}
+            className={multiModuleStyles(styles.paymentitem, isActive ? styles.active : "")}
+            onClick={onClick}
+            {...props}
+        >
+            <div className={styles.title}>{title}</div>
+            <div className={styles.description}>
+                {description}
+                {picture && (
+                    <svg
+                        version="1.1"
+                        id="Layer_1"
+                        xmlns="http://www.w3.org/2000/svg"
+                        x="16px"
+                        y="16px"
+                        viewBox="0 0 8000 400"
+                    >
+                        <g id="Page-1">
+                            <g id="Artboard" transform="translate(-91.000000, -154.000000)">
+                                <g id="Group" transform="translate(91.000000, 154.000000)">
+                                    <path
+                                        id="Combined-Shape"
+                                        style={{ fill: "#37a72e" }}
+                                        d="M544.1,240.5v108h60v-64h68c28.6-0.2,52.9-18.5,62.1-44H544.1z"
+                                    />
+                                    <path
+                                        id="Combined-Shape_1_"
+                                        style={{ fill: "#00A0E5" }}
+                                        d="M536.1,151.5c3.5,44.1,45.3,79,96.3,79c0.2,0,104.3,0,104.3,0
+c0.8-4,1.2-8.2,1.2-12.5c0-36.6-29.5-66.2-66-66.5L536.1,151.5z"
+                                    />
+                                    <path
+                                        id="Combined-Shape_2_"
+                                        style={{ fill: "#37a72e" }}
+                                        d="M447.3,229.4l0-0.1L447.3,229.4c0.7-1.2,1.8-1.9,3.2-1.9c2,0,3.5,1.6,3.6,3.5l0,0
+v116.5h60v-196h-60c-7.6,0.3-16.2,5.8-19.4,12.7L387,266.6c-0.1,0.4-0.3,0.8-0.5,1.2l0,0l0,0c-0.7,1-1.9,1.7-3.3,1.7
+c-2.2,0-4-1.8-4-4v-114h-60v196h60v0c7.5-0.4,15.9-5.9,19.1-12.7l49-105.1C447.2,229.6,447.3,229.5,447.3,229.4L447.3,229.4z"
+                                    />
+                                    <path
+                                        id="Combined-Shape_3_"
+                                        style={{ fill: "#37a72e" }}
+                                        d="M223.3,232.8l-35.1,114.7H145L110,232.7c-0.3-1.8-1.9-3.2-3.9-3.2
+c-2.2,0-3.9,1.8-3.9,3.9c0,0,0,0,0,0l0,114h-60v-196h51.5H109c11,0,22.6,8.6,25.8,19.1l29.2,95.5c1.5,4.8,3.8,4.7,5.3,0
+l29.2-95.5c3.2-10.6,14.8-19.1,25.8-19.1h15.3h51.5v196h-60v-114c0,0,0,0,0-0.1c0-2.2-1.8-3.9-3.9-3.9
+C225.2,229.5,223.6,230.9,223.3,232.8L223.3,232.8z"
+                                    />
+                                </g>
+                            </g>
+                        </g>
+                    </svg>
+                )}
+            </div>
+        </div>
+    );
+}
+function PaymentList() {
+    const components = [
+        { id: 1, title: "Банковской картой", description: "Оплата картой курьеру при получении заказа", picture: true },
+        { id: 2, title: "Онлайн", description: "Безопасная оплата банковской картой онлайн", picture: true },
+        { id: 3, title: "Наличными", description: "Оплата наличными курьеру при получении заказа", picture: false },
+    ];
+    const [selectedId, setSelectedId] = useState(null);
+    const handleSelect = id => {
+        if (selectedId === id) {
+            setSelectedId(null);
+            return;
+        }
+        setSelectedId(id);
+    };
+    return (
+        <div className={styles.payment}>
+            {components.map(elem => {
+                return (
+                    <PaymentItem
+                        title={elem.title}
+                        description={elem.description}
+                        picture={elem.picture}
+                        onClick={() => {
+                            handleSelect(elem.id);
+                        }}
+                        isActive={selectedId === elem.id}
+                    />
+                );
+            })}
+        </div>
+    );
+}
